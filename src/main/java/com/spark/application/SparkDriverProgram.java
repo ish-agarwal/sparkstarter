7034ae2b3e64354ed174ff45485b06e2dfe52ec0
ish-agarwal <ish.agarwal.1@gmail.com>
2020-09-17 13:57:24 +0530

    SparkContext Spark Session

--- src/main/java/com/spark/application/SparkDriverProgram.java
+++ src/main/java/com/spark/application/SparkDriverProgram.java
@@ -1,13 +1,8 @@
 package com.spark.application;
 
-import org.apache.commons.codec.Encoder;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.*;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import org.apache.spark.sql.SparkSession;
 
 public class SparkDriverProgram {
     public static void main(String[] args) {
@@ -15,11 +10,11 @@ public class SparkDriverProgram {
         SparkSession spark = SparkSession.builder().master("local[*]").appName("Java Spark SQL Example").getOrCreate();
         System.out.println("Version : " + spark.version());
 
-//        //Spark 1.0
-//        SparkConf sc = new SparkConf().setAppName("Driver Program").setMaster("local[2]");
-//        JavaSparkContext jsc = new JavaSparkContext(sc);
-//        System.out.println("Partitions 1.0 : " + jsc.defaultMinPartitions());
-//        jsc.close();
+        //Spark 1.0
+        SparkConf sc = new SparkConf().setAppName("Driver Program").setMaster("local[2]");
+        JavaSparkContext jsc = new JavaSparkContext(sc);
+        System.out.println("Partitions 1.0 : " + jsc.defaultMinPartitions());
+        jsc.close();
 
         //Spark2.0
         JavaSparkContext jsc2 = JavaSparkContext.fromSparkContext(spark.sparkContext());
