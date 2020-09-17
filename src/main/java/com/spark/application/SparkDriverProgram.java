d5465a942c0b6caceab6c0f1f3a92444132e3058
ish-agarwal <ish.agarwal.1@gmail.com>
2020-09-17 13:56:47 +0530

    SparkContext Spark Session

--- src/main/java/com/spark/application/SparkDriverProgram.java
+++ src/main/java/com/spark/application/SparkDriverProgram.java
@@ -1,22 +1,30 @@
 package com.spark.application;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.commons.codec.Encoder;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class SparkDriverProgram {
     public static void main(String[] args) {
-//        SparkConf sparkConf = new SparkConf()
-//                .setAppName("Example Spark App")
-//                .setMaster("local[*]"); // Delete this line when submitting to a cluster
-//        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
-//        JavaRDD<String> stringJavaRDD = sparkContext.textFile("D:\\TestData\\Spark\\nationalparks.csv");
-//        JavaRDD<Object> rowRDD = stringJavaRDD.map(RowFactory::create);
-//        System.out.println("Number of lines in file = " + stringJavaRDD.count());
 
         SparkSession spark = SparkSession.builder().master("local[*]").appName("Java Spark SQL Example").getOrCreate();
-        Dataset<Row> df = spark.read().csv("D:\\TestData\\Spark\\nationalparks.csv");
-        df.show();
+        System.out.println("Version : " + spark.version());
+
+//        //Spark 1.0
+//        SparkConf sc = new SparkConf().setAppName("Driver Program").setMaster("local[2]");
+//        JavaSparkContext jsc = new JavaSparkContext(sc);
+//        System.out.println("Partitions 1.0 : " + jsc.defaultMinPartitions());
+//        jsc.close();
+
+        //Spark2.0
+        JavaSparkContext jsc2 = JavaSparkContext.fromSparkContext(spark.sparkContext());
+        System.out.println("Partitions 2.0 : " + jsc2.defaultMinPartitions());
+        jsc2.close();
     }
 
 }
